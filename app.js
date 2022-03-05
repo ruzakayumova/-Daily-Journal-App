674b037ea7b9453fe56fe537c7b74851648dfaee
ruzakayumova <feruza.abdukayumova00@gmail.com>
2022-03-05 19:11:01 +0500

    added date and heading fields

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // selectors
+const headingInput = document.getElementById('heading-input')
+const dateInput = document.getElementById('date-input')
 const mainInput = document.getElementById('main-input')
 const createBtn = document.getElementById('create-btn')
 const container = document.querySelector('.container')
@@ -16,15 +18,24 @@ function createNote() {
     //create div card-header
     const cardHeader = document.createElement('div')
     cardHeader.classList.add("card-header")
+    cardHeader.innerText = dateInput.value
     cardDiv.appendChild(cardHeader)
+    dateInput.value = ""
     // create div card -body
     const cardBody = document.createElement('div')
     cardBody.classList.add("card-body")
     cardDiv.appendChild(cardBody)
+    //create h4 card-title
+    const cardTitle = document.createElement('h4')
+    cardTitle.classList.add('card-title')
+    cardTitle.innerText = headingInput.value
+    cardBody.appendChild(cardTitle)
+    headingInput.value = ""
     // create p card-text
     const cardText = document.createElement('p')
     cardText.classList.add("card-text")
     cardText.innerText = mainInput.value
     cardBody.appendChild(cardText)
+    mainInput.value = ""
 }
 
