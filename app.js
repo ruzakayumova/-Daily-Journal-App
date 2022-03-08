179a16d064bd315b5da7058b9240e85819473393
ruzakayumova <feruza.abdukayumova00@gmail.com>
2022-03-08 21:45:46 +0500

    local storage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const createBtn = document.getElementById('create-btn')
 const container = document.querySelector('.container')
 
 // events
+document.addEventListener('DOMContentLoaded', getNotes)
 createBtn.addEventListener('click', createNote)
 
 //functions
@@ -20,7 +21,7 @@ function createNote() {
     cardHeader.classList.add("card-header")
     cardHeader.innerText = dateInput.value
     cardDiv.appendChild(cardHeader)
-    dateInput.value = ""
+    
     // create div card -body
     const cardBody = document.createElement('div')
     cardBody.classList.add("card-body")
@@ -30,12 +31,77 @@ function createNote() {
     cardTitle.classList.add('card-title')
     cardTitle.innerText = headingInput.value
     cardBody.appendChild(cardTitle)
-    headingInput.value = ""
     // create p card-text
     const cardText = document.createElement('p')
     cardText.classList.add("card-text")
     cardText.innerText = mainInput.value
     cardBody.appendChild(cardText)
+
+    //add to localStorage
+    saveLocalNotes()
+
+    dateInput.value = ""
+    headingInput.value = ""
     mainInput.value = ""
 }
 
+
+//Local storage
+
+let myObj = {
+    date: dateInput.value,
+    title: headingInput.value,
+    text: mainInput.value
+}
+
+function saveLocalNotes() {
+    //check if there is already a list there
+    let notes
+    if(localStorage.getItem('notes') === null){
+        notes = []
+    } else {
+        notes = JSON.parse(localStorage.getItem('notes'))
+    }
+      notes.push(myObj);
+      localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+
+
+
+function getNotes() {
+    let notes
+    if(localStorage.getItem('notes') === null){
+        notes = []
+    } else {
+        notes = JSON.parse(localStorage.getItem('notes'))
+    }
+    notes.forEach((myObj) => {
+        //create div card
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add("card", "border-primary", "mb-3", "mt-2")
+        cardDiv.style.maxWidth = '100%'
+        container.appendChild(cardDiv)
+        //create div card-header
+        const cardHeader = document.createElement('div')
+        cardHeader.classList.add("card-header")
+        cardHeader.innerText = myObj.date
+        cardDiv.appendChild(cardHeader)
+        
+        // create div card -body
+        const cardBody = document.createElement('div')
+        cardBody.classList.add("card-body")
+        cardDiv.appendChild(cardBody)
+        //create h4 card-title
+        const cardTitle = document.createElement('h4')
+        cardTitle.classList.add('card-title')
+        cardTitle.innerText = myObj.title
+        cardBody.appendChild(cardTitle)
+        // create p card-text
+        const cardText = document.createElement('p')
+        cardText.classList.add("card-text")
+        cardText.innerText = myObj.text
+        cardBody.appendChild(cardText)
+
+    })
+}
